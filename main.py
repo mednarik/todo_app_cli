fb932a4c453f0d645033db45d33a6fd4d3137f0e
HomePC <michi.bednarik@gmail.com>
2025-12-26 18:06:54 +0100

    added recursive task adding and function to clear the done list

--- main.py
+++ main.py
@@ -6,24 +6,30 @@ with open("data.json", "r") as f:
    data = json.load(f)
 
 
-toDoList = data["toDo"]
 doneList = data["done"]
 
 underline_bold = "\033[1;4m"
 reset = "\033[0m"
 
+navigation_stack = [data["toDo"]]
 
+
+
+def update_file():
+   with open("data.json", "w") as f:
+      json.dump(data, f, indent=4)
 
 def show_tasks():
-   max_rows = max(len(toDoList), len(doneList))
+   max_rows = max(len(navigation_stack[-1]), len(doneList))
    tab = 50
+   current_level = navigation_stack[-1]
 
    print(f"{underline_bold}{f"To Do":<{tab}}{f"|Done":<{tab}}{reset}")
 
-   if toDoList == []:
+   if current_level == []:
       todo_text = "No tasks have been added yet"
    else:
-      todo_text = f"1. {toDoList[0]['name']}"
+      todo_text = f"1. {current_level[0]['name']}"
 
    if doneList == []:
       done_text = "No tasks have been completed yet"
@@ -33,8 +39,8 @@ def show_tasks():
    print(f"{todo_text:<{tab}}|{done_text}")
    
    for i in range(1, max_rows):
-      if i < len(toDoList):
-         todo_text = f"{i+1}. {toDoList[i]['name']}"
+      if i < len(current_level):
+         todo_text = f"{i+1}. {current_level[i]['name']}"
       else:
          todo_text = ""
       
@@ -45,7 +51,7 @@ def show_tasks():
       
       print(f"{todo_text:<{tab}}|{done_text}")
 
-   return bool(toDoList)
+   return bool(current_level)
 
    
 
@@ -58,22 +64,22 @@ def add_task():
    now = datetime.now()
    now = now.strftime("%Y-%m-%d %H:%M:%S")
 
-   toDoList.append({"name": name, "description": "", "date":now, "subtasks":[]})
+   navigation_stack[-1].append({"name": name, "description": "", "date":now, "subtasks":[]})
 
    
    #add the data to the json file
-   with open("data.json", "w") as f: 
-      json.dump(data, f, indent=4)
+   update_file()
       
 
 def remove_task():
    if(show_tasks()):
+      current_level = navigation_stack[-1]
       
       while True:
          try:
             index = int(input(">>> ")) - 1
-            if 0 <= index < len(toDoList):
-               toDoList.pop(index)
+            if 0 <= index < len(current_level):
+               current_level.pop(index)
                break
             
             else:
@@ -82,13 +88,13 @@ def remove_task():
          except Exception:
             input("Not a valid index...")
 
-      with open("data.json", "w") as f:
-         json.dump(data, f, indent=4)
+      update_file()
 
 def change_order():
    #show the tasks
    if(show_tasks()):
 
+      current_level = navigation_stack[-1]
 
       #enter the number of the task to move
       print("Enter the index of the task you want to move:")
@@ -97,16 +103,16 @@ def change_order():
       try:
          index = int(index) - 1 
 
-         item = toDoList[index]
+         item = current_level[index]
 
          #highlight it somehow
          while True:
             os.system("cls")
-            for i in range(len(toDoList)):
+            for i in range(len(current_level)):
                if i == index:
-                  print(f"\033[1;43m{str(i+1)}. {data['toDo'][index]['name']}\033[0m")
+                  print(f"\033[1;43m{str(i+1)}. {current_level[index]['name']}\033[0m")
                else:
-                  print(str(i+1) + ". " + toDoList[i]["name"])
+                  print(str(i+1) + ". " + current_level[i]["name"])
             print("\nUse 'W' to move up and 'S' to move down / press enter only to stop")
             
             action = input(">>> ")
@@ -116,46 +122,68 @@ def change_order():
 
             elif action.lower() == "w" and 0 < index:
             
-               toDoList.pop(index)
+               current_level.pop(index)
                index -= 1
-               toDoList.insert(index, item)
+               current_level.insert(index, item)
                
 
-            elif action.lower() == "s" and index < len(toDoList) - 1:
+            elif action.lower() == "s" and index < len(current_level) - 1:
                
-               toDoList.pop(index)
+               current_level.pop(index)
                index += 1
-               toDoList.insert(index, item)
+               current_level.insert(index, item)
       except Exception:
          input("Not a valid index...")
 
-         with open("data.json", "w") as f:
-               json.dump(data, f, indent=4)
+      update_file()
+         
 
-def add_subtasks():
+def open_task():
    if(show_tasks()):
-      print("Enter the index of the task you want to add a subtask to:")
       try:
-         index = int(input(">>> ")) - 1
+            
+         print("")
+         print(f"\n{underline_bold}Controls{reset}")
+         print('Enter the task index to go inside')
+         print('Enter A/a to go back')
+         user_input = input(">>> ")
 
          os.system("cls")
 
+         if user_input.lower() == "a":
+            if len(navigation_stack) > 1:
+               navigation_stack.pop()
+            else:
+               input("Already at root level...")
+         
+         
+         else:
+            index = int(user_input) - 1 
+            navigation_stack.append(navigation_stack[-1][index]["subtasks"])
 
-         with open("data.json", "w") as f:
-            json.dump(data, f, indent=4)
+            
 
       except IndexError:
-         input("Not a valid index...")
+         input("Invalid index (Index out of range)...")
+
+      except ValueError:
+         input("Invalid index (Indices must be numbers)...")
          
 def move_to_doneList():
    if(show_tasks()):
+      current_level = navigation_stack[-1]
       print("Enter the index of the task you have competed:")
       index = int(input(">>> ")) - 1
-      doneList.append(toDoList[index])
-      toDoList.pop(index)
+      doneList.append(current_level[index])
+      current_level.pop(index)
 
-      with open("data.json", "w") as f:
-         json.dump(data, f, indent=4)
+      update_file()
+
+def clear_done():
+
+   doneList.clear()
+   update_file()
+
 #handle the controls
 while True:
    os.system("cls")
@@ -167,8 +195,9 @@ while True:
    print("Enter 2 to remove a task")
    print("Enter 3 to quit the program")
    print("Enter 4 to change the order")
-   print("Enter 5 to add a subtask")
+   print("Enter 5 to open a task")
    print("Enter 6 to move a task from To Do to Done")
+   print("Enter 7 to clear Done")
    action = input(">>> ")
    if action == "1":
       os.system("cls")
@@ -187,11 +216,15 @@ while True:
    
    elif action =="5":
       os.system("cls")
-      add_subtasks()
+      open_task()
    
    elif action =="6":
       os.system("cls")
       move_to_doneList()
+
+   elif action =="7":
+      os.system("cls")
+      clear_done()
       
    else:
       input("Not a valid action...")
